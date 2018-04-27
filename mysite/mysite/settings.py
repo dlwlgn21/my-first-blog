bccea3e65facd5e220e108273e7ed87f11b6352f
dlwlgn21 <dlwlgn213@gmail.com>
2018-04-27 23:55:17 +0900

    LastVersion

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '(i9e7j*25rp0)4a_24zr^b@4e5o=q&)x&!#%4g!2974lz&q)ze'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
